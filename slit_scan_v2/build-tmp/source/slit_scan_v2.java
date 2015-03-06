b095a29e56861725110288463b37e2b793cedbd3
felixstr <fexfix@bluewin.ch>
2015-03-06 10:22:32 +0100

    v2 mit hashMap und buffer-cleaner

---
+++ slit_scan_v2/build-tmp/source/slit_scan_v2.java
@@ -0,0 +1,142 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import processing.video.*; 
+import java.util.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class slit_scan_v2 extends PApplet {
+
+
+
+
+Capture video;
+int rowHeight = 1;
+float rowDelay = 10;
+boolean topToBottom = true;
+
+int frameNumber = 0;
+
+HashMap<Integer, PImage> frameBuffer = new HashMap<Integer, PImage>();
+
+public void setup() {
+	size(640*3/2, 480*3/2);
+
+	video = new Capture(this, 640, 480, 30);
+	video.start();
+}
+
+public void draw() {
+	background(0);
+
+	// println(frameCount);
+
+	if (video.available()) {
+		video.read();
+	}
+	
+
+	video.loadPixels();
+	readFrame();
+
+	pushMatrix();
+		scale(-1.66f, 1.66f);
+		translate(-640, 0);
+		drawImage();	
+
+	popMatrix();
+
+	frameNumber++;
+}	
+
+public void readFrame() {
+	frameBuffer.put(frameNumber, video.get());
+}
+
+public void drawImage() {
+
+	// image(frameBuffer.get(frameNumber), 0, 0);
+
+	
+	int top = 0;
+	float frameDelayStep = (rowDelay/1000* frameRate);
+	
+	
+
+	int step = 0;
+	int frameDelay = 0;
+
+	while (top < height) {
+		frameDelay = PApplet.parseInt(frameNumber - (frameDelayStep * step));
+		// println("frameDelay: "+frameDelay);
+		if (frameDelay > 0 && frameBuffer.get(frameDelay) != null) {
+			int imageTop = top;
+			if (!topToBottom) {
+				imageTop = height-top-rowHeight;
+			}
+
+			PImage frameImage = frameBuffer.get(frameDelay).get(0, imageTop, width, rowHeight);
+
+			image(frameImage, 0, imageTop);
+		}
+
+
+
+		
+		top += rowHeight;
+		step++;
+	}
+
+	bufferClean(frameDelay);
+	
+
+
+	// println("frameBuffer Size: "+frameBuffer.size());
+
+
+}
+
+public void bufferClean(int frameDelay) {
+	// anzahlrows * delay
+	if (frameBuffer.get(frameDelay-1) != null) {
+		ArrayList<Integer> deleteElements = new ArrayList<Integer>();
+
+
+		Iterator itr = frameBuffer.entrySet().iterator();
+		while(itr.hasNext()) {
+			Map.Entry mapEntry = (Map.Entry)itr.next();
+			int bufferFrameNumber = (Integer)mapEntry.getKey();
+
+			if (bufferFrameNumber < frameDelay-50) {
+				deleteElements.add(bufferFrameNumber);
+				
+			}
+
+		}
+
+		for (int i = 0; i < deleteElements.size(); i++) {
+			frameBuffer.remove(deleteElements.get(i));
+			// println("deleted: "+deleteElements.get(i));
+		}
+	}
+
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "slit_scan_v2" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
---
+++ slit_scan_v2/slit_scan_v2.pde
@@ -0,0 +1,114 @@
+import processing.video.*;
+import java.util.*;
+
+Capture video;
+int rowHeight = 1;
+float rowDelay = 10;
+boolean topToBottom = true;
+
+int frameNumber = 0;
+
+HashMap<Integer, PImage> frameBuffer = new HashMap<Integer, PImage>();
+
+void setup() {
+	size(640*3/2, 480*3/2);
+
+	video = new Capture(this, 640, 480, 30);
+	video.start();
+}
+
+void draw() {
+	background(0);
+
+	// println(frameCount);
+
+	if (video.available()) {
+		video.read();
+	}
+	
+
+	video.loadPixels();
+	readFrame();
+
+	pushMatrix();
+		scale(-1.66, 1.66);
+		translate(-640, 0);
+		drawImage();	
+
+	popMatrix();
+
+	frameNumber++;
+}	
+
+void readFrame() {
+	frameBuffer.put(frameNumber, video.get());
+}
+
+void drawImage() {
+
+	// image(frameBuffer.get(frameNumber), 0, 0);
+
+	
+	int top = 0;
+	float frameDelayStep = (rowDelay/1000* frameRate);
+	
+	
+
+	int step = 0;
+	int frameDelay = 0;
+
+	while (top < height) {
+		frameDelay = int(frameNumber - (frameDelayStep * step));
+		// println("frameDelay: "+frameDelay);
+		if (frameDelay > 0 && frameBuffer.get(frameDelay) != null) {
+			int imageTop = top;
+			if (!topToBottom) {
+				imageTop = height-top-rowHeight;
+			}
+
+			PImage frameImage = frameBuffer.get(frameDelay).get(0, imageTop, width, rowHeight);
+
+			image(frameImage, 0, imageTop);
+		}
+
+
+
+		
+		top += rowHeight;
+		step++;
+	}
+
+	bufferClean(frameDelay);
+	
+
+
+	// println("frameBuffer Size: "+frameBuffer.size());
+
+
+}
+
+void bufferClean(int frameDelay) {
+	// anzahlrows * delay
+	if (frameBuffer.get(frameDelay-1) != null) {
+		ArrayList<Integer> deleteElements = new ArrayList<Integer>();
+
+
+		Iterator itr = frameBuffer.entrySet().iterator();
+		while(itr.hasNext()) {
+			Map.Entry mapEntry = (Map.Entry)itr.next();
+			int bufferFrameNumber = (Integer)mapEntry.getKey();
+
+			if (bufferFrameNumber < frameDelay-50) {
+				deleteElements.add(bufferFrameNumber);
+				
+			}
+
+		}
+
+		for (int i = 0; i < deleteElements.size(); i++) {
+			frameBuffer.remove(deleteElements.get(i));
+			// println("deleted: "+deleteElements.get(i));
+		}
+	}
+
+}
